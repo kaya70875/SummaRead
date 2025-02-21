31822c435f06e5ed204a4788c964c37063051e9b
Ahmet Kaya <shortsforturkey@gmail.com>
2025-02-21 12:24:47 +0300

    create a src folder for project base

--- scripts/getOptions.js
+++
@@ -1,8 +0,0 @@
-export const getR = () => {
-  return new Promise((resolve) => {
-    chrome.storage.sync.get(["summaryLength"], async (result) => {
-      const sliderValue = result.summaryLength || 200;
-      resolve(sliderValue);
-    });
-  });
-};
