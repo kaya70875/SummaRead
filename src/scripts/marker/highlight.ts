75d17773007b7525f5cec379412714da344041de
Ahmet Kaya <shortsforturkey@gmail.com>
2025-02-22 22:11:41 +0300

    update highlight.js

--- src/scripts/marker/highlight.ts
+++ src/scripts/marker/highlight.ts
@@ -1,14 +1,11 @@
 import Mark from "mark.js";
 import { SummaryResponse } from "../../types/api";
-console.log("Highlight script loaded.");
 
 // Function to highlight the summary using Mark.js
-const highlightSummary = (data: SummaryResponse) => {
-  console.log("Highlighting summary...");
+const injectMarks = (data: SummaryResponse) => {
   const sentencesToHighlight = data.summary;
 
   const markInstance = new Mark(document.body);
-  console.log("Mark.js instance created");
   markInstance.mark(sentencesToHighlight, {
     element: "span",
     className: "highlight",
@@ -24,11 +21,10 @@ const highlightSummary = (data: SummaryResponse) => {
 
 // Function to remove highlights, for instance by removing the added span tags
 // (You may need a more robust method depending on your implementation)
-const removeHighlights = () => {
+const removeMarks = () => {
   // Simple example: remove all elements with class "highlight"
-  document
-    .querySelectorAll("span.highlight")
-    .forEach((el) => el.classList.remove("highlight"));
+  const markInstance = new Mark(document.body);
+  markInstance.unmark();
 };
 
-export { highlightSummary, removeHighlights };
+export { injectMarks, removeMarks };
