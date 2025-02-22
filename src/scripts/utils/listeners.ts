9e48be098bb66310593ed5a5b4f296f0d5ffc328
Ahmet Kaya <shortsforturkey@gmail.com>
2025-02-22 22:10:30 +0300

    feat: add listener for storage changes to communicate with active tab

---
+++ src/scripts/utils/listeners.ts
@@ -0,0 +1,28 @@
+type NewState = string | number | boolean;
+
+/**
+ *
+ * @param newState : The new state to be sent to the tab
+ * @param stateName  : The name of the state to be sent to the tab this can be get like request.bgColor
+ * @param action : The action to be sent eventListener takes this like request.action
+ */
+export const listenStorageChanges = async (
+  newState: string,
+  stateName: string,
+  action: string
+) => {
+  const [tab] = await chrome.tabs.query({
+    active: true,
+    currentWindow: true,
+  });
+  try {
+    if (tab?.id) {
+      chrome.tabs.sendMessage(tab.id, {
+        action: action,
+        [stateName]: newState as NewState,
+      });
+    }
+  } catch (error) {
+    console.error("Error sending message to tab:", error);
+  }
+};
