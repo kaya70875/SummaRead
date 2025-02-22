d74a2c34eaf9a8b151256de3eb3fbba89cffe9b6
Ahmet Kaya <shortsforturkey@gmail.com>
2025-02-22 22:10:40 +0300

    simplify storage change handling by using a dedicated listener function

--- src/background.ts
+++ src/background.ts
@@ -1,30 +1,18 @@
+import { listenStorageChanges } from "./scripts/utils/listeners.js";
+
 chrome.storage.onChanged.addListener(async (changes, namespace) => {
   if (namespace === "sync" && changes.buttonActive) {
     const newState = changes.buttonActive.newValue;
-
-    const [tab] = await chrome.tabs.query({
-      active: true,
-      currentWindow: true,
-    });
-    if (tab?.id) {
-      chrome.tabs.sendMessage(tab.id, {
-        action: "updateButtonState",
-        state: newState,
-      });
-    }
+    await listenStorageChanges(newState, "state", "updateButtonState");
   } else if (namespace === "sync" && changes.summaryLength) {
     const newState = changes.summaryLength.newValue;
-
-    const [tab] = await chrome.tabs.query({
-      active: true,
-      currentWindow: true,
-    });
-    if (tab?.id) {
-      chrome.tabs.sendMessage(tab.id, {
-        action: "updateSliderState",
-        state: newState,
-      });
-    }
+    await listenStorageChanges(newState, "state", "updateSliderState");
+  } else if (namespace === "sync" && changes.backgroundColor) {
+    const newState = changes.backgroundColor.newValue;
+    await listenStorageChanges(newState, "bgColor", "updateColorPref");
+  } else if (namespace === "sync" && changes.textColor) {
+    const newState = changes.textColor.newValue;
+    await listenStorageChanges(newState, "textColor", "updateColorPref");
   }
 });
 
