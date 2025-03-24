4b4548e6516a4e0a5d362266efe6663d1c88db7a
Ahmet Kaya <shortsforturkey@gmail.com>
2025-03-24 18:19:45 +0300

    refactor fetchSummary function to support multiple HTML tags for text extraction

--- src/scripts/utils/fetch.ts
+++ src/scripts/utils/fetch.ts
@@ -1,7 +1,14 @@
+// Tags thats going to be fetched.
+const tags = ["p", "li"];
+
 // Function to fetch the summary as before
 export const fetchSummary = async (rValue: number) => {
-  const raw_text = Array.from(document.querySelectorAll("p"))
-    .map((p) => p.innerText)
+  const raw_text = tags
+    .flatMap((tag) =>
+      Array.from(document.querySelectorAll(tag)).map(
+        (element) => element.textContent
+      )
+    )
     .join(" ");
 
   const response = await fetch("http://127.0.0.1:8000/summarize", {
