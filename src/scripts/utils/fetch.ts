73738d9b84b03b14274fd71464f73bda772bff64
Ahmet Kaya <shortsforturkey@gmail.com>
2025-03-26 16:52:59 +0300

    update fetch

--- src/scripts/utils/fetch.ts
+++ src/scripts/utils/fetch.ts
@@ -6,16 +6,19 @@ export const fetchSummary = async (rValue: number) => {
   const article = new Readability(documentClone).parse();
   const raw_text = article ? article.textContent : "";
 
-  const response = await fetch(`http://127.0.0.1:8000/summarize`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      paragraph: raw_text,
-      r: rValue || 0.3,
-    }),
-  });
+  const response = await fetch(
+    `https://summarizer-production-ec90.up.railway.app/summarize`,
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        paragraph: raw_text,
+        r: rValue || 0.3,
+      }),
+    }
+  );
   const data = await response.json();
   return data;
 };
